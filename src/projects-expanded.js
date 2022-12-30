01cf1fccfe05e86f55ca54b63e52c235b0bef1d8
Taikwy <tyler.wang@darkwindmedia.com>
2022-12-30 13:11:28 -0800

    modal anims

--- src/projects-expanded.js
+++ src/projects-expanded.js
@@ -18,28 +18,64 @@ const closeModals = document.querySelectorAll(".close-button");
 for(let i = 0; i < modals.length; i++){
   openModals[i].addEventListener("click", () => {
     console.log("open");
+    modals[i].classList.remove('hide');
     modals[i].showModal();
     // document.body.style.overflow = 'hidden';
+
+    
   });
   closeModals[i].addEventListener("click", () => {
     console.log("close");
-    modals[i].close();
+    modals[i].classList.add('hide');
+    // modals[i].close();
+    // document.body.style.overflow = 'scroll';
+
+    
+  });
+
+  modals[i].addEventListener("animationend", function() {
+    if(modals[i].classList.contains('hide'))
+      modals[i].close();
   });
 }
 
+circle.addEventListener("animationend", function() {
+  change.innerHTML = "The animation has ended!";
+});
 
-const dialogs = document.querySelectorAll("dialog");
+// document.querySelector('#close').onclick = function() {
+//   dialog.classList.add('hide');
+//   dialog.addEventListener('webkitAnimationEnd', function(){
+//       dialog.classList.remove('hide');
+//       dialog.close();
+//       dialog.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+//   }, false);
+// };
 
-for(let i = 0; i < dialogs.length; i++){
-  dialogs[i].addEventListener('click', (e) => {
-    console.log(e.target);
-    if (e.target === dialogs[i]) {
-      // dialogs[i].close();
-    }
-    if (!$(e.target).closest(".modal,open-button").length) {
-      console.log(e.target);
-      // modal.close();
-      // $("body").find(".modal").removeClass("visible");
-    }
-  });
-}
+
+// $('.button').click(function(){
+//   var buttonId = $(this).attr('id');
+//   $('#modal-container').removeAttr('class').addClass(buttonId);
+//   $('body').addClass('modal-active');
+// })
+
+// $('#modal-container').click(function(){
+//   $(this).addClass('out');
+//   $('body').removeClass('modal-active');
+// });
+
+// const dialogs = document.querySelectorAll("dialog");
+
+// for(let i = 0; i < dialogs.length; i++){
+//   dialogs[i].addEventListener('click', (e) => {
+//     console.log(e.target);
+//     if (e.target === dialogs[i]) {
+//       // dialogs[i].close();
+//     }
+//     if (!$(e.target).closest(".modal,open-button").length) {
+//       console.log(e.target);
+//       // modal.close();
+//       // $("body").find(".modal").removeClass("visible");
+//     }
+//   });
+// }
