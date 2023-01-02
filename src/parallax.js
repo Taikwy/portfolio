f67ceb94cbd47200f7e0450d00fccb364477afec
Taikwy <tyler.wang@darkwindmedia.com>
2023-01-01 16:27:04 -0800

    drawing sketches

--- src/parallax.js
+++ src/parallax.js
@@ -1,4 +1,8 @@
-document.addEventListener("mousemove", parallax);
+document.addEventListener("mousemove", landingParallax);
+document.addEventListener("mousemove", setupParallax);
+document.addEventListener("mousemove", hobbyParallax);
+document.addEventListener("mousemove", gamingParallax);
+
 function parallax(e){
     document.querySelectorAll(".object").forEach(function(move){
         var moving_value = move.getAttribute("data-value");
@@ -6,6 +10,8 @@ function parallax(e){
         var ySpeed = move.getAttribute("ySpeed");
         var zLayer = move.getAttribute("zLayer");
 
+        if(xSpeed ==0 || ySpeed == 0)
+            return;
         
 
         var xToCenter = e.pageX - window.innerWidth/2;
@@ -13,6 +19,12 @@ function parallax(e){
         
         var x = (window.innerWidth - xToCenter * xSpeed * zLayer)/100;
         var y = (window.innerHeight - yToCenter * ySpeed * zLayer)/100;
+
+        var x = (window.innerWidth - xToCenter * xSpeed * zLayer);
+        var y = (window.innerHeight - yToCenter * ySpeed * zLayer);
+        
+        var x = (xToCenter * xSpeed * zLayer)/100;
+        var y = (yToCenter * ySpeed * zLayer)/100;
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px) ";
         
         // move.style.transform = "translate(50%, 50%)";
@@ -25,5 +37,62 @@ function parallax(e){
 
         // var x = (window.innerWidth - xToCenter * moving_value)/100;
         // var y = (window.innerHeight - yToCenter * moving_value)/100;
+    });
+}
+
+function landingParallax(e){
+    document.querySelectorAll(".landing .object").forEach(function(move){
+        var xSpeed = move.getAttribute("xSpeed");
+        var ySpeed = move.getAttribute("ySpeed");
+        var zLayer = move.getAttribute("zLayer");
+
+        var xToCenter = e.pageX - window.innerWidth/2;
+        var yToCenter = e.pageY - window.innerHeight/2;
+        
+        var x = (xToCenter * xSpeed * zLayer)/100;
+        var y = (yToCenter * ySpeed * zLayer)/100;
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px) ";
+    });
+}
+function setupParallax(e){
+    document.querySelectorAll(".setup .object").forEach(function(move){
+        var xSpeed = move.getAttribute("xSpeed");
+        var ySpeed = move.getAttribute("ySpeed");
+        var zLayer = move.getAttribute("zLayer");
+
+        var xToCenter = e.pageX - window.innerWidth/2;
+        var yToCenter = e.pageY - window.innerHeight/2;
+        
+        var x = (xToCenter * xSpeed * zLayer)/100;
+        var y = (yToCenter * ySpeed * zLayer)/100;
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px) ";
+    });
+}
+function hobbyParallax(e){
+    document.querySelectorAll(".hobbies .object").forEach(function(move){
+        var xSpeed = move.getAttribute("xSpeed");
+        var ySpeed = move.getAttribute("ySpeed");
+        var zLayer = move.getAttribute("zLayer");
+
+        var xToCenter = e.pageX - window.innerWidth/2;
+        var yToCenter = e.pageY - window.innerHeight/2;
+        
+        var x = (xToCenter * xSpeed * zLayer)/100;
+        var y = (yToCenter * ySpeed * zLayer)/100;
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px) ";
+    });
+}
+function gamingParallax(e){
+    document.querySelectorAll(".gaming .object").forEach(function(move){
+        var xSpeed = move.getAttribute("xSpeed");
+        var ySpeed = move.getAttribute("ySpeed");
+        var zLayer = move.getAttribute("zLayer");
+
+        var xToCenter = e.pageX - window.innerWidth/2;
+        var yToCenter = e.pageY - window.innerHeight/2;
+        
+        var x = (xToCenter * xSpeed * zLayer)/100;
+        var y = (yToCenter * ySpeed * zLayer)/100;
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px) ";
     });
 }
